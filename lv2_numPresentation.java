c9d4ff569aa56ea5627f4675c7403591f49de614
Kim YeongRae <dudfo3728@gmail.com>
2021-05-07 21:16:26 +0900

    20210506
    숫자의 표현

---
+++ lv2_numPresentation.java
@@ -0,0 +1,38 @@
+package lv2_20210506;
+
+public class lv2_numPresentation 
+{
+	public static int solution(int n) 
+    {
+        int answer = 0;
+        int sum = 0;
+        
+        for(int i=1; i<=n; i++)
+        {
+            sum = 0;
+            
+            for(int j=i; j<=n; j++)
+            {
+                sum += j;
+                
+                if(sum==n)
+                {
+                    sum = 0;
+                    answer++;
+                    break;
+                }
+                if(sum>n)
+                {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+	
+    public static void main(String[] args)
+    {
+    	System.out.println(solution(15));
+    }
+}
