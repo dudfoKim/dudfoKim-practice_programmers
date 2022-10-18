59d28dd0410892a89dc8928f51e24f9cf2aa8a11
Kim YeongRae <dudfo3728@gmail.com>
2022-10-18 14:17:51 +0900

    Create 숫자 짝꿍.java
    프로그래머스 레벨1 숫자 짝꿍

---
+++ 숫자 짝꿍.java
@@ -0,0 +1,55 @@
+
+
+==========================================
+  
+import java.util.TreeMap;
+
+class Solution
+{
+    public String solution(String X, String Y)
+    {
+        StringBuilder sb = new StringBuilder();
+        boolean change = false;
+        int[] valuex = new int[10];
+        int[] valuey = new int[10];
+        
+        for(int i=0; i<X.length(); i++)
+        {
+            valuex[(int)X.charAt(i)-'0']++;
+        }
+        for(int i=0; i<Y.length(); i++)
+        {
+            valuey[(int)Y.charAt(i)-'0']++;
+        }
+        for(int i=valuex.length-1; i>=0; i--)
+        {
+            int cnt = 0;
+            int num = 0;
+            
+            if(valuex[i]!=0 && valuey[i]!=0)
+            {
+                cnt = Math.min(valuex[i], valuey[i]);
+                num = valuex[i];
+                
+                for(int j=0; j<cnt; j++)
+                {
+                    sb.append(i);
+                }
+                
+                change = true;
+            }
+        }
+        if(!change)
+        {
+            return "-1";
+        }
+        else if(sb.toString().charAt(0)=='0')
+        {
+            return "0";
+        }
+        else
+        {
+            return sb.toString();
+        }
+    }
+}
