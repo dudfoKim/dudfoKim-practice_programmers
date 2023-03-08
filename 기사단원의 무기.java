67f7ace09c5861734bb58e672b097c20c6358e54
Kim YeongRae <dudfo3728@gmail.com>
2023-03-08 11:11:09 +0900

    Create 기사단원의 무기.java
    프로그래머스 레벨1 기사단원의 무기

---
+++ 기사단원의 무기.java
@@ -0,0 +1,43 @@
+https://yoloaeee.tistory.com/304
+
+====================================
+  
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int number, int limit, int power)
+    {
+        int answer = 0;
+        int[] arr = new int[number+1];
+        
+        for(int i=1; i<=number; i++)
+        {
+            arr[i]++;
+            
+            for(int j=1; j<=i/2; j++)
+            {
+                if(i%j==0)
+                {
+                    arr[i]++;
+                }
+            }
+        }
+        
+        Arrays.sort(arr);
+        
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]>limit)
+            {
+                answer += power;
+            }
+            else
+            {
+                answer += arr[i];
+            }
+        }
+        
+        return answer;
+    }
+}
